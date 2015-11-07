ae08fd668b74f4a0806d97a0ef60cc2015d36288
josdem <joseluis.delacruz@gmail.com>
2015-11-07 13:10:54 -0600

    Completnado ejemplo de palabras palindromes

--- ValidatorExample.groovy
+++ ValidatorExample.groovy
@@ -1,7 +1,11 @@
-def rightExecution(s, closure){
+def isPalindrome (s, closure){
   if(closure(s)){
     println "Right"
   } else {
     println "Wrong"
   }
 }
+
+def validate  = { it == it.reverse() }
+def word = "anitalavalatina"
+isPalindrome(word, validate)
