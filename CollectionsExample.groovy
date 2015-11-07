eacf974dd2fd1cc5d4d3b950e791dec0271e6229
josdem <joseluis.delacruz@gmail.com>
2015-11-07 13:29:29 -0600

    Agregando mas ejemplos de uso de collections

--- CollectionsExample.groovy
+++ CollectionsExample.groovy
@@ -4,3 +4,18 @@ collection = ('a'..'z')
 
 println collection
 
+//Iterando una lista
+def list = [1,2,3,4,5]
+list.each { println it * 3}
+
+//Agregando elementos a una collection
+def otherList = [1,2,3]
+otherList.add(4)
+otherList << 5
+otherList << 6 << 7
+intersectList = [3,4]
+
+println otherList
+println otherList - [1,2]
+println otherList.intersect(intersectList)
+
