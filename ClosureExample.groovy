8f9d78b053234e7edecbacfdc5c0210a18e5fd4f
josdem <joseluis.delacruz@gmail.com>
2015-11-07 12:59:01 -0600

    Se amplia la explicacion y alcance con un closure

--- ClosureExample.groovy
+++ ClosureExample.groovy
@@ -1,4 +1,5 @@
 //Un closure es un bloque de codigo, que puede ser tratado como un objeto que define una estructura de codigo con un valor de retorno.
+//El pase de parametros de un closure es por referencia.
 def multiplo(n, closure){
   closure(n)
 }
