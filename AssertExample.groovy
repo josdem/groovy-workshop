d0fc601e308a31f063de39b0f052c7b574d6d0a5
josdem <joseluis.delacruz@gmail.com>
2015-11-07 12:42:15 -0600

    Agregando mas ejemplos como elvis y null safe

--- AssertExample.groovy
+++ AssertExample.groovy
@@ -1,2 +1,23 @@
+//Assert siempre evalua a Groovy truth
+//False es considerado 0 o null o cadena vacía
 def n = 12
-assert n==12
+assert n == 12
+
+def hola = ''
+assert !hola
+
+//Operador ternario
+def z = 'hola'
+x = z ? z : 'otra cosa'
+println x
+
+//Operador Elvis
+def y = 'hola'
+x = y ?: 'otra cosa'
+println x
+
+//Null safe
+//En esta expresion valida que la variable nullSafe no sea null, si no lo es ejecuta size.
+def nullSafe = null
+println nullSafe?.size().toString()
+
