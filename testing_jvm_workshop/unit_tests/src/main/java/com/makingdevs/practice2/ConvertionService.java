cc6f7f218f42738ceca1deb3512ebb58f123ec3a
josdem <joseluis.delacruz@gmail.com>
2016-03-11 09:01:00 -0600

    Adding testing jvm

---
+++ testing_jvm_workshop/unit_tests/src/main/java/com/makingdevs/practice2/ConvertionService.java
@@ -0,0 +1,10 @@
+package com.makingdevs.practice2;
+
+public class ConvertionService {
+
+	private WeatherWebService weatherService;
+	
+	public float retrieveWeatherInCelsiusFromCountryAndCity(String country, String city){
+		return 0f;
+	} 
+}
